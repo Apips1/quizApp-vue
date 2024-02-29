906f6af07b4cc1576817cb35549f96f355bee085
Afif Nugroho <92508998+Apips1@users.noreply.github.com>
2024-02-29 22:44:15 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+<RouterView/>
+<!-- <QuizesView/> -->
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import QuizesView from './views/QuizesView.vue'
+
+
+</script>
+
+<style scoped>
+  .container {
+    max-width: 1000px;
+    margin: 0 auto
+  }
+
+</style>
